be87871a1f5938784b1b67bf8600854eba11b1b1
Jan Dvořák <jan.dvorak@palaxo.com>
2017-11-05 11:53:57 +0100

    Přidejte soubory projektu.

---
+++ CTSliceReconstruction/Bitmap.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTSliceReconstruction
+{
+    public class Bitmap
+    {
+
+        public double this[int i, int j]
+        {
+            get { return 0.0; }
+            set { }
+        }
+
+        double this[Point point]
+        {
+            get { return 0.0; }
+            set { }
+        }
+    }
+}
---
+++ CTSliceReconstruction/ProjectionHandler.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTSliceReconstruction
+{
+    public abstract class ProjectionHandler
+    {
+        public List<double> GenerateProjections(Bitmap bmp, int sliceCount)
+        {
+            return null;
+        }
+
+        public double[] CreateProjection(Bitmap bmp, double angle)
+        {
+            return null;
+        }
+
+        public Bitmap ExtrudeProjection(double[] projection, double angle)
+        {
+            return null;
+        }
+
+        protected 
+    }
+}
