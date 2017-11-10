cfa12f339d7b11e12fcbe7b800ef6da83115d1ee
Jan Dvořák <jdvorak@students.zcu.cz>
2017-11-10 11:30:43 +0100

    bresenham line generator class design

---
+++ CTSliceReconstruction/ProjectionHandlerBresenham.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTSliceReconstruction
+{
+    class ProjectionHandlerBresenham : ProjectionHandler
+    {
+        protected override List<PixelInfo> generateLine(double angle, int n, int position)
+        {
+            //Generate line
+
+            //Calculate intersections with bounding box
+
+            //Calculate intersected pixels on boundary
+
+            return null;
+        }
+
+        public List<PixelInfo> bresenhamLine(int x0, int y0, int x1, int y1)
+        {
+            return null;
+        }
+
+        private List<PixelInfo> VerticalLine(int x, int n)
+        {
+            List<PixelInfo> result = new List<PixelInfo>();
+
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(new PixelInfo(x, i, 1.0));
+            }
+
+            return result;
+        }
+
+        private List<PixelInfo> HorizontalLine(int y, int n)
+        {
+            List<PixelInfo> result = new List<PixelInfo>();
+
+            for (int i = 0; i < n; i++)
+            {
+                result.Add(new PixelInfo(i, y, 1.0));
+            }
+
+            return result;
+        }
+    }
+}
